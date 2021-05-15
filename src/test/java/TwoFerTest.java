b9975438e745295e912c2eaaf618186e570af92c
Angelica <Angelica137@users.noreply.github.com>
2021-05-15 16:58:53 +0100

    refactor test for no name given

--- src/test/java/TwoFerTest.java
+++ src/test/java/TwoFerTest.java
@@ -1,10 +1,23 @@
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.Before;
 
 public class TwoFerTest {
+
+	private TwoFer twofer;
+
+	@Before
+	public void setup() {
+		twofer = new TwoFer();
+	}
+
 	@Test
 	public void testThatTwoFerReturnsCorrectStringNoName() {
-		assertEquals("One for you, one for me.", new TwoFer().getTwoFer(null));
+		String input = null;
+		String expected = "One for you, one for me.";
+
+		assertEquals(expected, twofer.getTwoFer(input));
 	}
 
 	public void testThatTwoFerReturnsCorrectStringWithName() {
